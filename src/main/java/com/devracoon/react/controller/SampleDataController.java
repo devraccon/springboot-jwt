4e8ce509bca18ee8e2cdad834e620574d171aa3e
devracoon <73985363+devraccon@users.noreply.github.com>
2021-01-06 13:11:37 +0900

    Update SampleDataController.java

--- src/main/java/com/devracoon/react/controller/SampleDataController.java
+++ src/main/java/com/devracoon/react/controller/SampleDataController.java
@@ -26,7 +26,7 @@ public class SampleDataController {
 		for(int i = 0 ; i < 20 ; i++) {
 			SampleDataVO data = new SampleDataVO();
 			data.setId("ID_"+ i);
-			data.setName("이름_"+ i);
+			data.setName("Name!!!!"+ i);
 			data.setPhone("010_000_"+ i);
 			data.setAddr("ADDR_"+ i);
 			sampleDatas.add(data);
